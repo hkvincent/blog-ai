6b1e9aa4057d7e329948afdcb34c47072ec0d272
vincent <hkvincentchan@gmail.com>
2023-08-23 23:36:21 +0800

    new post side bar

--- app/post/layout.tsx
+++ app/post/layout.tsx
@@ -1,14 +1,20 @@
 import LoginInfo from '@/components/loginInfo';
+import Link from 'next/link';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoins } from '@fortawesome/free-solid-svg-icons';
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
             <div className="flex flex-col text-white overflow-hidden">
                 <div className="bg-slate-800 px-2 ">
-                    <div> first row</div>
-                    <div>  second </div>
-                    <div>  third </div>
+                    <div> logo </div>
+                    <Link href="/post/new" className="btn">  new post </Link>
+                    <Link href="/post/token-topup" className=" mt-6 mb-4 text-center flex items-center gap-2 justify-center">
+                        <FontAwesomeIcon icon={faCoins} className="text-yellow-500 align-middle w-6" size="sm" />
+                        <span className="align-middle">0 tokens available</span>
+                    </Link>
                 </div>
                 <div className="px-4 flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
                     <div> list row</div>
