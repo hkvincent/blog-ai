151fd4232f9812d173dfa3fefcbe988475f177d1
vincent <hkvincentchan@gmail.com>
2023-10-06 21:31:11 +0800

    add payment success page

--- app/post/success/page.tsx
+++ app/post/success/page.tsx
@@ -1,7 +1,16 @@
-export default function Page() {
-    return (
-        <div>
-            <h1>Success, thank you for your pruchase!</h1>
-        </div>
-    );
-}
+import React from 'react';
+import Link from 'next/link';
+
+const Page = () => {
+  return (
+    <div className="container mx-auto mt-20 text-center">
+      <h1 className="text-4xl font-bold mb-4">Payment Successful!</h1>
+      <p className="text-lg mb-8">Thank you for your purchase.</p>
+      <Link href="/post/new">
+        <a className="text-blue-500 underline">Go back to New Post</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Page;
