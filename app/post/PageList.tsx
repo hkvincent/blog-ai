d50229aa4c45a348a1f43d5af5c52d1c6720be80
vincent <hkvincentchan@gmail.com>
2023-10-07 00:16:22 +0800

    add loading gif

--- app/post/PageList.tsx
+++ app/post/PageList.tsx
@@ -7,7 +7,7 @@ import { useParams } from 'next/navigation';
 const PageList = ({ postList, hasMorePosts }: { postList: any[], hasMorePosts: boolean }) => {
     const { postId } = useParams();
     const { setPosts, posts, getPosts, noMorePosts, setHasMorePosts } = useContext(PostsContext)!;
-
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
         setPosts(postList);
         if (postId) {
@@ -29,19 +29,48 @@ const PageList = ({ postList, hasMorePosts }: { postList: any[], hasMorePosts: b
         }
     }, [setHasMorePosts, hasMorePosts, postId, posts, postList]);
 
+    const loadMorePosts = async () => {
+        setIsLoading(true);
+        await getPosts({ lastPostId: posts[posts.length - 1]._id });
+        setIsLoading(false);
+    };
+
     return (
         <>
-            {posts.map((post: any) => (
+            {posts.map((post) => (
                 <PostItem post={post} key={post._id} />
             ))}
             {!noMorePosts && (
                 <div
-                    onClick={() => {
-                        getPosts({ lastPostId: posts[posts.length - 1]._id });
-                    }}
+                    onClick={loadMorePosts}
                     className="hover:underline text-sm text-slate-400 text-center cursor-pointer mt-4"
                 >
-                    Load more posts
+                    {isLoading ? (
+                        <div className="flex justify-center items-center">
+                            <svg
+                                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                            >
+                                <circle
+                                    className="opacity-25"
+                                    cx="12"
+                                    cy="12"
+                                    r="10"
+                                    stroke="currentColor"
+                                    strokeWidth="4"
+                                ></circle>
+                                <path
+                                    className="opacity-75"
+                                    fill="currentColor"
+                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 1.37.259 2.678.741 3.824l1.505-1.533z"
+                                ></path>
+                            </svg>
+                        </div>
+                    ) : (
+                        'Load more posts'
+                    )}
                 </div>
             )}
         </>
