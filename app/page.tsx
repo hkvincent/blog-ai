e700c99a3107dfaea60e23994df3bd764dd740a1
vincent <hkvincentchan@gmail.com>
2023-10-07 09:18:55 +0800

    update content

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Index() {
       <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-800/90 rounded-md backdrop-blur-sm">
         <Logo />
         <p>
-          The AI-powered SAAS solution to generate SEO-optimized blog posts in
+          The AI-powered SAAS solution to generate blog posts in
           minutes. Get high-quality content, without sacrificing your time.
         </p>
         <Link href="/post/new" className="btn">
