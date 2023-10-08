f50814ef552d80ff2501db6d56de5743f4395d70
vincent <hkvincentchan@gmail.com>
2023-10-08 12:18:10 +0800

    fix load more issue

--- app/api/getPosts/route.ts
+++ app/api/getPosts/route.ts
@@ -15,9 +15,6 @@ export const POST = withApiAuthRequired(async function (request: NextRequest) {
     try {
         const { user }: { user: any } = (await getSession()) || { user: undefined };
         const { selectedPostId, lastPostId, searchTerm } = await request.json();
-        console.log('selectedPostId', selectedPostId);
-        console.log('lastPostId', lastPostId);
-        console.log('searchTerm', searchTerm);
 
         const client = await clientPromise;
         const db = client.db('BlogStandard');
